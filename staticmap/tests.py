85bdb26f3a054f8e4926d113a1e0c574c2a7d88f
Sam <sam@samr1.net>
2023-07-14 19:02:20 +0200

    fix rendering by replacing Image.ANTIALIAS with Image.LANCZOS

--- staticmap/tests.py
+++ staticmap/tests.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
+from unittest.mock import patch
 
+from PIL import Image
+
+from staticmap import Line, StaticMap
 from staticmap.staticmap import _lat_to_y, _lon_to_x, _y_to_lat, _x_to_lon
 
 
@@ -17,3 +21,23 @@ class LonLatConversionTest(TestCase):
                 y = _lat_to_y(lat, zoom)
                 l = _y_to_lat(y, zoom)
                 self.assertAlmostEqual(lat, l, places=5)
+
+
+class RenderTest(TestCase):
+    @staticmethod
+    def test_renders_calls_resize_with_LANCZOS():
+        # Image.ANTIALIAS have been removed in Pillow 10+
+        # https://pillow.readthedocs.io/en/stable/releasenotes/10.0.0.html#constants
+        width = 400
+        height = 25
+        m = StaticMap(width, height)
+        m.headers = {"User-Agent": "Mozilla 5.0"}
+        m.add_line(Line([(13.4, 52.5), (2.3, 48.9)], "blue", 3))
+        with patch.object(
+            Image.Image,
+            "resize",
+            side_effect=[Image.new('RGBA', (width, height), (255, 0, 0, 0))]
+        ) as resize_mock:
+            m.render()
+
+        resize_mock.assert_called_with((width, height), Image.LANCZOS)
